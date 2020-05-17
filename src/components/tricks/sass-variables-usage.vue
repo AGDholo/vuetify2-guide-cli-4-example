b4e4f4988c8419924eaee0c64dc26f98a312a3f2
AGD <agdholo@gmail.com>
2020-05-18 07:46:59 +0800

    feat(tricks): create sass variables usage

---
+++ src/components/tricks/sass-variables-usage.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- 项目的基础根字体现在为 30px -->
+  <div>
+    $font-size-root: 30px;
+  </div>
+</template>
---
+++ src/components/tricks/theme-usage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- 使用 color 属性添加颜色 -->
+    <v-btn color="primary">
+      primary color
+    </v-btn>
+
+    <!-- 使用 background-color 属性添加颜色 -->
+    <v-tabs background-color="primary" color="accent">
+      <v-tab>Item One</v-tab>
+      <v-tab>Item Two</v-tab>
+      <v-tab>Item Three</v-tab>
+    </v-tabs>
+
+    <!-- 类名方法设置颜色 -->
+    <v-btn class="primary">
+      primary color
+    </v-btn>
+
+    <!-- 设置文字颜色 -->
+    <v-btn class="primary accent--text">
+      text color
+    </v-btn>
+
+    <!-- 设置颜色亮度 -->
+    <v-btn class="primary darken-4">
+      primary color
+    </v-btn>
+
+    <!-- 设置文字颜色亮度 -->
+    <v-btn class="primary accent--text text--darken-4">
+      text color
+    </v-btn>
+  </div>
+</template>
