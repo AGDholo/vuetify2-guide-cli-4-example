361eaf2fe33012eff0e738d534132668ee0ef9ad
AGD <agdholo@gmail.com>
2020-05-20 08:04:23 +0800

    feat(layout): create center-content-vertically

---
+++ src/components/layout/center-content-vertically-usage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- fill-height 属性，将区块内容设置为 100%，这样才能够让页面可以在垂直方向居中 -->
+    <!-- 你也可以将 fill-height 作为 class 使用，以便应用到任何类上面，class="fill-height" -->
+    <v-container fill-height>
+      <!-- align 属性，将内容块设置为垂直方向居中 -->
+      <!-- justify 属性，将内容块设置为横向方向居中 -->
+      <!-- 更多用例请查看官方文档 -->
+      <v-row align="center" justify="center">
+        <v-col>
+          center
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
